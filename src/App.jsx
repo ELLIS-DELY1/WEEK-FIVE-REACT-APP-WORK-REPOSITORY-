c2837d1f83a655327d07223e1822d394d3f5cf7e
ELLIS-DELY1 <nexauragency@gmail.com>
2025-04-27 14:25:47 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a simple React component that displays content using JSX.</p>
+      <img 
+        src="https://via.placeholder.com/150" 
+        alt="Placeholder Image"
+      />
+    </div>
+  );
+}
+
+export default App;
